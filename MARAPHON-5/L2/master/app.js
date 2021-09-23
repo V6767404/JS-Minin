9467bf3bd17830c28bbdb734a4b75956a92c94dd
V6767404 <32267531+V6767404@users.noreply.github.com>
2021-09-24 01:26:19 +0300

    commit

--- MARAPHON-5/L2/master/app.js
+++ MARAPHON-5/L2/master/app.js
@@ -1,3 +1,27 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
-console.log(item)
+// console.log(item)
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for(const placeholder of placeholders){
+    placeholder.addEventListener('dragover')
+    placeholder.addEventListener('dragenter')
+    placeholder.addEventListener('dragleave')
+    placeholder.addEventListener('drop')
+}
+
+function dragstart(event) {
+//    console.log('drag start', event.target)
+    event.target.classList.add('hold')
+    setTimeout(() => event.target.classList.add
+    ('hide'), 0)
+}
+
+function dragend(event) {
+//    console.log('drag end')
+    event.target.classList.remove('hold', 'hide')
+   
+}
