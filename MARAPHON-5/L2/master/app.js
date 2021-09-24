30129340110897104ec9d0a41e28fda61c41d0af
V6767404 <32267531+V6767404@users.noreply.github.com>
2021-09-24 13:17:58 +0300

    commit

--- MARAPHON-5/L2/master/app.js
+++ MARAPHON-5/L2/master/app.js
@@ -7,21 +7,41 @@ item.addEventListener('dragstart', dragstart)
 item.addEventListener('dragend', dragend)
 
 for(const placeholder of placeholders){
-    placeholder.addEventListener('dragover')
-    placeholder.addEventListener('dragenter')
-    placeholder.addEventListener('dragleave')
-    placeholder.addEventListener('drop')
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
 }
 
 function dragstart(event) {
-//    console.log('drag start', event.target)
+// console.log('drag start', event.target)
     event.target.classList.add('hold')
     setTimeout(() => event.target.classList.add
     ('hide'), 0)
 }
 
 function dragend(event) {
-//    console.log('drag end')
+// console.log('drag end')
     event.target.classList.remove('hold', 'hide')
    
 }
+
+function dragover(event) {
+    console.log('drag over')
+           
+    }
+
+function dragenter(event) {
+    console.log('drag enter')
+    event.target.classList.add('hovered')
+           
+    }
+function dragleave(event) {
+    console.log('drag leave')
+           
+    }
+
+function dragdrop(event) {
+    console.log('drag drop')
+           
+    }
