2914dcbdf01ff80b48a1ee1e429bf0d0d3dfa4bd
V6767404 <32267531+V6767404@users.noreply.github.com>
2022-01-05 12:54:58 +0300

    commit

--- RS School/codewars/codewars.js
+++ RS School/codewars/codewars.js
@@ -64,4 +64,14 @@ const century = year => Math.ceil(year/100)
 function century(year) {
     return (year + 99) / 100 | 0;
 }
+//
+function century(year) {
+    if (year <= 100){
+        return 1;
+    }
+    let cen = parseInt(year / 100);
+    let rem = year % 100;
 
+    return rem === 0 ? cen : cen + 1;
+}
+//
